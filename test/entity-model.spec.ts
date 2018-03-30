cda90ebe119de2e60c2d2c67cc3b40fbd81f9a3f
Guangda Zhang <guangda@prosperworks.com>
2018-03-29 22:44:12 -0700

    add more test

--- test/entity-model.spec.ts
+++ test/entity-model.spec.ts
@@ -1,21 +1,31 @@
 import { assert, expect } from 'chai';
 import sinon from 'sinon';
-import EntityModel, { IContextData } from '../src/entity-model';
+import EntityModel from '../src/entity-model';
 
 describe('entity-model', function () {
-  let onSaveSpy: (model: EntityModel) => Promise<IContextData>;
+  let onSaveSpy: sinon.SinonSpy;
   beforeEach(function () {
     onSaveSpy = sinon.spy();
   });
 
-  it('should create a model correctly', function () {
-    const model = new EntityModel(
-      'person',
-      { id: 1, name: 'Alice', age: 21},
-      ['age'],
-      onSaveSpy,
-    );
-
+  it('should create a model correctly', async function () {
+    const obj = {
+      id: 1,
+      name: 'Alice',
+      age: 21,
+      gender: 'female',
+    };
+    const model = new EntityModel('person', obj, ['age'], onSaveSpy);
+    expect(() => {
+      (model as any).type = 'yo';
+    }).to.throw();
     expect(model.type).to.equal('person');
+    expect((model as any).name).to.equal('Alice');
+
+    expect(model.toObject()).to.eql({ ...obj });
+    expect(model.toJSON()).to.equal(JSON.stringify(obj));
+
+    const data = await model.save();
+    assert(onSaveSpy.calledOnce);
   });
 });
